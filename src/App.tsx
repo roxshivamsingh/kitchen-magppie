0628c7257471a450c6592e97b7571e44aed525e3
Shivam Singh <roxshivamsingh@gmail.com>
2024-06-02 02:10:50 +0530

    FIX: Comment the auth code to work on the project;

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
 import { useFirebaseAuth } from "./hooks/firebase/use-firebase-listeners"
 import Routes from "./routes"
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
-import { useLocation, useNavigate } from 'react-router-dom';
-import { RouteEnum } from "./types/route";
-import CustomCircularProgress from "./components/custom-circular-progress";
+// import { useLocation, useNavigate } from 'react-router-dom';
+// import { RouteEnum } from "./types/route";
+// import CustomCircularProgress from "./components/custom-circular-progress";
 export default function App() {
 
     const auth = useFirebaseAuth()
-    const navigate = useNavigate()
-    const { pathname } = useLocation()
+    console.log(auth?.value)
+    // const navigate = useNavigate()
+    // const { pathname } = useLocation()
 
-    useEffect(() => {
-        if (!(auth.loading || auth?.value)) {
-            if (RouteEnum.SignIn !== pathname)
-                navigate(`/${RouteEnum.SignIn}`)
-        }
-    }, [auth.loading, auth?.value, navigate, pathname])
+    // useEffect(() => {
+    //     if (!(auth.loading || auth?.value)) {
+    //         if (RouteEnum.SignIn !== pathname)
+    //             navigate(`/${RouteEnum.SignIn}`)
+    //     }
+    // }, [auth.loading, auth?.value, navigate, pathname])
 
 
 
-    if (auth?.loading) {
+    // if (auth?.loading) {
 
-        return <CustomCircularProgress />
-    }
+    //     return <CustomCircularProgress />
+    // }
     return <Routes />
 }
 
