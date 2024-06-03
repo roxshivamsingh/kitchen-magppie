8d50d9f6746ca07fbf8dc3f06232ce7cc57948f6
Shivam Singh <roxshivamsingh@gmail.com>
2024-06-03 11:00:15 +0530

    ADD: Signin firebase pipeline;

--- src/containers/sign-in/sign-in.tsx
+++ src/containers/sign-in/sign-in.tsx
@@ -2,29 +2,15 @@
 
 import React from 'react'
 import kitchenlogin from '../../assets/videos/kitchen-login.mp4'
-// import { useForm, SubmitHandler } from 'react-hook-form'
-
-// type Inputs = {
-//     email: string
-//     passowrd: string
-// }
-
+import * as yup from "yup"
+import { useForm } from "react-hook-form"
+import { useNavigate } from "react-router-dom"
+import { yupResolver } from "@hookform/resolvers/yup"
+import { useFirebaseActionAuth } from '../../hooks/firebase/use-firebase-actions'
 const Login: React.FC = () => {
-    // const {
-    //     register,
-    //     handleSubmit,
-    //     watch,
-    //     formState: { errors },
-    // } = useForm<Inputs>()
-
-    // const handleSubmit = (SubmitHandler<Inputs> = (
-    //     event: React.FormEvent,
-    //     data
-    // ) => {
-    //     event.preventDefault()
-    //     console.log(data)
-    // })
-
+    const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(schema) })
+    const AuthAction = useFirebaseActionAuth()
+    const navigate = useNavigate()
     return (
         <div className="min-h-screen flex md:flex-row">
             <div className="hidden md:block md:w-3.8/4 md:h-auto">
@@ -41,7 +27,11 @@ const Login: React.FC = () => {
             <div className="w-full md:w-1.2/4 flex items-center justify-center bg-gray-100 min-h-screen">
                 <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-lg rounded-lg">
                     <h2 className="text-2xl font-bold text-center">Login</h2>
-                    <form onSubmit="" className="space-y-4">
+                    <form onSubmit={handleSubmit((data) => {
+                        AuthAction.signIn(data).then((e) => {
+                            navigate('/')
+                        })
+                    })} className="space-y-4">
                         <div>
                             <label
                                 htmlFor="email"
@@ -53,6 +43,7 @@ const Login: React.FC = () => {
                                 id="email"
                                 name="email"
                                 type="email"
+                                {...register('email')}
                                 required
                                 className="mt-1 w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
                             // value={email}
@@ -69,6 +60,8 @@ const Login: React.FC = () => {
                             <input
                                 id="password"
                                 name="password"
+                                {...register('password')}
+
                                 type="password"
                                 required
                                 className="mt-1 w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
@@ -92,3 +85,9 @@ const Login: React.FC = () => {
 }
 
 export default Login
+
+type TFormInput = { email: string, password: string }
+const schema = yup.object({
+    email: yup.string().email().required('Email is required'),
+    password: yup.string().required('Password is required')
+})
