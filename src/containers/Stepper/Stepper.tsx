0daad5f97f27c6dd28c5672385c4dbaffc3c2016
Shivam Singh <roxshivamsingh@gmail.com>
2024-06-10 00:18:17 +0530

    ADD: Stepper below content;

--- src/containers/Stepper/Stepper.tsx
+++ src/containers/Stepper/Stepper.tsx
@@ -1,32 +1,47 @@
+import { useMemo } from 'react'
 import review from '../../assets/photos/review.png'
 import Sidebar from '../home/Hero/components/Sidebar'
+import g1 from "../../assets/photos/kitchen/g1.png";
+import g2 from "../../assets/photos/kitchen/g2.png";
+import g3 from "../../assets/photos/kitchen/g3.png";
+import g4 from "../../assets/photos/kitchen/g4.png";
+import g5 from "../../assets/photos/kitchen/g5.png";
+import g6 from "../../assets/photos/kitchen/g6.png";
+import g7 from "../../assets/photos/kitchen/g7.png";
+import g8 from "../../assets/photos/kitchen/g8.png";
+import g9 from "../../assets/photos/kitchen/g9.png";
+import g10 from "../../assets/photos/kitchen/g10.png";
+import g11 from "../../assets/photos/kitchen/g11.png";
+import g12 from "../../assets/photos/kitchen/g12.png";
+import g13 from "../../assets/photos/kitchen/g13.png";
+import g14 from "../../assets/photos/kitchen/g14.png";
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default function Stepper() {
-    return (<div
-        className={`relative min-h-screen bg-cover bg-center`}
-        style={{ backgroundImage: `url(${review})` }}
-    >
 
-        <div className="container lg:mx-auto pt-10 p-3 ">
-            <div className="text-5xl uppercase text-white font-extrabold lg:my-10 md:my-10">
-                MAGPPIE
+    const renderCards = useMemo(() => (<div className="grid lg:grid-flow-col lg:grid-cols-4 md:grid-flow-col md:grid-cols-3 gap-4 my-10 text-sm lg:text-lg uppercase">
+        {STEPPER_OPTIONS?.map((row, i) => (<div key={i}>
+            <div className="shadow-lg bg-white rounded-md p-4 min-h-full">
+                <div className="flex gap-2 flex-row align-middle py-1">
+                    <div className="my-auto">Step</div>
+                    <div className='border px-2 rounded-2xl border-black'>{i + 1}</div>
+                </div>
+                <div className='py-1'>CHOOSE YOUR <span className='font-bold'>KITCHEN {row}</span></div>
             </div>
-            <div className="text-2xl text-white font-extrabold my-5">
-                Get a Quote in 3 easy steps
-            </div>
-            <div className="grid lg:grid-flow-col lg:grid-cols-4 md:grid-flow-col md:grid-cols-3 gap-4 my-10 text-sm lg:text-lg uppercase">
-                {STEPPER_OPTIONS?.map((row, i) => (<div key={i}>
-                    <div className="shadow-lg bg-white rounded-md p-4 min-h-full">
-                        <div className="flex gap-2 flex-row align-middle py-1">
-                            <div className="my-auto">Step</div>
-                            <div className='border px-2 rounded-2xl border-black'>{i + 1}</div>
-                        </div>
-                        <div className='py-1'>CHOOSE YOUR <span className='font-bold'>KITCHEN {row}</span></div>
-                    </div>
-                </div>))}
-            </div>
-        </div>
-
+        </div>))}
+    </div>), [])
+    const renderFooter = useMemo(() => (<>
         <div className="absolute top-0 min-h-screen right-0 invisible lg:visible md:visible">
             <Sidebar />
         </div>
@@ -38,10 +53,56 @@ export default function Stepper() {
                 </div>
                 <div className="py-1 ">Scroll down to select a style</div>
             </div>
-
         </div>
-    </ div>)
+    </>), [])
+
+    return (
+        <div className="bg-[#434343]">
+
+            <div className='relative min-h-screen bg-cover bg-center' style={{ backgroundImage }}>
+                <div className="container lg:mx-auto pt-10 p-3 ">
+                    <div className="text-5xl uppercase text-white font-extrabold lg:my-10 md:my-10">
+                        MAGPPIE
+                    </div>
+                    <div className="text-2xl text-white font-extrabold my-5">
+                        Get a Quote in 3 easy steps
+                    </div>
+                    {renderCards}
+                </div>
+
+                {renderFooter}
+            </div>
+            <div className="text-white uppercase">
+
+                <div className="my-4 container mx-auto">
+                    <div className="flex gap-2 flex-row align-middle py-1">
+                        <div className="my-auto">Step</div>
+                        <div className='border px-2 rounded-2xl bg-white text-black'>1</div>
+                    </div>
+                    <div className='py-1'>CHOOSE YOUR <span className='font-bold'>KITCHEN Style</span></div>
+
+                </div>
+                <div className="container mx-auto">
+                    <div className="grid grid-cols-2  gap-4">
+                        {KITCHEN_OPTIONS?.map((image, i) => (
+                            <div key={i}>
+                                <img
+                                    className="h-auto max-w-full rounded-lg"
+                                    src={image}
+                                    alt=""
+                                />
+                            </div>
+                        ))}
+
+                    </div>
+
+                </div>
+            </div>
+
+        </div>)
 
 }
 
 const STEPPER_OPTIONS = ['Style', 'Size', 'Shape']
+const backgroundImage = `url(${review})`
+const KITCHEN_OPTIONS = [g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14]
