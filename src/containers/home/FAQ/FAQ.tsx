f2a8b1da0765b6ec8dca49c0c268eb2b7134c041
sonicxz <abhionic@outlook.com>
2024-06-10 01:44:29 +0530

    Accordian bug fix

--- src/containers/home/FAQ/FAQ.tsx
+++ src/containers/home/FAQ/FAQ.tsx
@@ -1,23 +1,30 @@
 import { Accordion } from 'flowbite-react'
 import { faqData } from './data'
+import './FAQ.css'
 
 export default function FAQ() {
     return (
-        <Accordion className="p-20">
+        <div className="p-20">
             <h1 className="text-4xl font-medium mb-10">FAQ</h1>
-            <Accordion.Panel>
-                {faqData.map((data) => (
-                    <div key={data.id} className="mb-10">
-                        <Accordion.Title>{data.title}</Accordion.Title>
-                        <Accordion.Content>
-                            <p className="mb-2 mt-2 text-gray-500">
-                                {data.description}
-                            </p>
-                        </Accordion.Content>
-                        <hr className='border' />
-                    </div>
+            <Accordion className="p-2" collapseAll>
+                {faqData.map((data, index) => (
+                    <>
+                        <Accordion.Panel>
+                            <div className="p-2">
+                                <Accordion.Title>{data.title}</Accordion.Title>
+                                <Accordion.Content>
+                                    <p className="mb-2 mt-2 text-gray-500">
+                                        {data.description}
+                                    </p>
+                                </Accordion.Content>
+                            </div>
+                        </Accordion.Panel>
+                        {index !== faqData.length - 1 && (
+                            <hr className="border" />
+                        )}
+                    </>
                 ))}
-            </Accordion.Panel>
-        </Accordion>
+            </Accordion>
+        </div>
     )
 }
