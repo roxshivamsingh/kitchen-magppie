139b4ce526be2a22c783221ccaf0252cd9e5978a
Shivam Singh <roxshivamsingh@gmail.com>
2024-06-11 17:21:40 +0530

    FIX: Drawer;

--- src/containers/home/Hero/components/Sidebar.tsx
+++ src/containers/home/Hero/components/Sidebar.tsx
@@ -1,8 +1,28 @@
+import { Drawer } from "flowbite-react"
+import { useState } from "react"
 import { NavLink } from "react-router-dom"
+import { Icon } from "@iconify/react"
 export default function Sidebar() {
-    return (
-        <>
-            <div className="w-70 h-screen text-white bg-transparent flex flex-col items-start p-4 border-l-2">
+
+    const [isLoginDrawerOpen, setIsLoginDrawerOpen] = useState(true)
+
+    return (<>
+
+        {!isLoginDrawerOpen && (<div className="absolute right-2 top-2 bg-[#434343] rounded-2xl p-1 text-white cursor-pointer"
+            onClick={() => { setIsLoginDrawerOpen(true) }}
+        >
+            <Icon icon='material-symbols:menu' width={40} />
+        </div>)}
+        <Drawer open={isLoginDrawerOpen} onClose={() => { setIsLoginDrawerOpen(false) }}
+            className="w-70 h-screen  bg-transparent p-4 border-l-2"
+            position="right"
+        >
+            {/* <Drawer.Header
+            className="text-2xl font-bold mb-4 border-b-2 w-full"
+            title="MENU"
+            titleIcon={() => <Icon icon='tabler:menu-deep' />}
+            /> */}
+            <Drawer.Items className="text-white flex flex-col items-start ">
                 <h2 className="text-2xl font-bold mb-4 border-b-2 w-full">
                     MENU
                 </h2>
@@ -18,7 +38,8 @@ export default function Sidebar() {
                 <NavLink className="mt-60 ml-12 text-lg" to='/sign-in'>
                     MEMBERS LOGIN
                 </NavLink>
-            </div>
-        </>
+            </Drawer.Items>
+        </Drawer>
+    </>
     )
 }
