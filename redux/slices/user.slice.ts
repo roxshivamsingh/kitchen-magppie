f1551158ab15fab88de7ec2a9c3e87b04a0918b2
Shivam Singh <roxshivamsingh@gmail.com>
2024-06-12 22:42:47 +0530

    FIX: Redux error;

--- redux/slices/user.slice.ts
+++ redux/slices/user.slice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-import { IUser } from '../../../types/user';
+import { IUser } from '../../types/user';
 
 interface IUserSlice {
     value?: IUser;
