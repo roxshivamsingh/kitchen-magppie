21411df38da030db3e94e8ced59f761230735d2a
AkshayKDev <akshay.official.dev@gmail.com>
2024-06-20 01:45:24 +0530

    more ui changes for cms

--- app/cms/containers/kitchens/KitchenCreateEdit/KitchenCreateEdit.tsx
+++ app/cms/containers/kitchens/KitchenCreateEdit/KitchenCreateEdit.tsx
@@ -1,69 +1,40 @@
-import * as yup from "yup"
-import { yupResolver } from "@hookform/resolvers/yup"
-import { useForm } from "react-hook-form"
+// import * as yup from 'yup'
+// import { yupResolver } from '@hookform/resolvers/yup'
+// import { useForm } from 'react-hook-form'
+
+import Form from '../components/Form'
+import Header from '../components/Header'
 
 export default function KitchenCreateEdit() {
+    // const {
+    //     handleSubmit,
+    //     // formState: { errors },
+    // } = useForm({ resolver: yupResolver(schema) })
 
-    const {
-        handleSubmit,
-        // formState: { errors },
-    } = useForm({ resolver: yupResolver(schema) })
-
-
-    const onSubmit = handleSubmit((data: TFormInput) => {
-        console.log(data)
-        // We'll add firebase pipeline here..
-    })
+    // const onSubmit = handleSubmit((data: TFormInput) => {
+    //     console.log(data)
+    //     // We'll add firebase pipeline here..
+    // })
 
     return (
-
-        <div className="container mx-auto">
-
-            <div className="text-3xl uppercase font-bold ">
-                Create Kitchen
+        <div>
+            <Header />
+            <div className="max-w-screen-xl flex flex-col justify-between mx-auto p-4 mt-4">
+                <h1 className="font-bold text-3xl mb-6">Add Kitchen</h1>
+                <Form />
             </div>
-            <form onSubmit={onSubmit}>
-                {['name', 'description']?.map((field, i) => (
-                    <div className="mb-4" key={i}>
-                        <input
-                            id="name"
-                            name="name"
-                            type="email"
-                            placeholder={field}
-                            className={`w-full capitalize text-xl border-t-0 border-l-0 border-r-0 border-gray-400`}
-                        />
-                    </div>
-                ))}
-                <button
-                    type='submit'
-                    // disabled={values?.loading}
-                    className={`grid grid-cols-3 flex-row align-middle w-full py-3 px-4 uppercase text-white  focus:outline-none focus:ring-2 bg-black focus:ring-offset-2`}
-
-                >
-                    <div className="" />
-                    <div className="">
-                        Create
-                    </div>
-                    <div className="">
-                        {/* <Icon icon='line-md:loading-loop' width={20} /> */}
-                    </div>
-                </button>
-
-            </form>
-
         </div>
-
     )
 }
 
-type TFormInput = {
-    name: string,
-    email: string,
-    city: string,
-    mobile: string,
-}
+// type TFormInput = {
+//     name: string,
+//     email: string,
+//     city: string,
+//     mobile: string,
+// }
 
-const schema = yup.object({
-    name: yup.string().required('Name is required'),
-    description: yup.string().required('Name is required'),
-})
+// const schema = yup.object({
+//     name: yup.string().required('Name is required'),
+//     description: yup.string().required('Name is required'),
+// })
--- app/cms/routes/routes.tsx
+++ app/cms/routes/routes.tsx
@@ -1,38 +1,49 @@
-import { RouteObject } from "react-router-dom";
-import CmsHome from "../containers/Home/Home";
+import { RouteObject } from 'react-router-dom'
+import CmsHome from '../containers/Home/Home'
 // import CmsHome from "../../cms/Home/Home";
 
-import KitchenCreateEdit from "../containers/kitchens/KitchenCreateEdit/KitchenCreateEdit";
-import CmsSignIn from "../containers/auth/SignIn/SignIn";
-import CmsKitchen from "../containers/kitchens/Kitchen"
-import AuthGuard from "../../../components/AuthGuard";
+import KitchenCreateEdit from '../containers/kitchens/KitchenCreateEdit/KitchenCreateEdit'
+import CmsSignIn from '../containers/auth/SignIn/SignIn'
+import CmsKitchen from '../containers/kitchens/Kitchen'
+import AuthGuard from '../../../components/AuthGuard'
+import Projects from '../containers/projects/Projects'
 const CmsRoutes = {
     path: 'cms',
     children: [
         {
-            element: (<AuthGuard variant="cms">
-                <CmsHome />
-            </AuthGuard>),
+            element: (
+                <AuthGuard variant="cms">
+                    <CmsHome />
+                </AuthGuard>
+            ),
             index: true,
         },
         {
             path: 'kitchen',
-            element: (<AuthGuard variant="cms">
-                <CmsKitchen />
-            </AuthGuard>),
+            element: (
+                <AuthGuard variant="cms">
+                    <CmsKitchen />
+                </AuthGuard>
+            ),
         },
         {
             path: 'kitchen/create',
-            element: (<AuthGuard variant="cms">
-                <KitchenCreateEdit />
-            </AuthGuard>),
+            element: (
+                <AuthGuard variant="cms">
+                    <KitchenCreateEdit />
+                </AuthGuard>
+            ),
         },
 
         {
             path: 'sign-in',
             element: <CmsSignIn />,
         },
-    ]
+        {
+            path: '/cms/projects',
+            element: <Projects />,
+        },
+    ],
 } as RouteObject
 
-export default CmsRoutes;
+export default CmsRoutes
