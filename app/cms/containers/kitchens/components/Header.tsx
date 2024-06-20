fee32c15661d09fab13f5a4bfea2078d633f7637
Shivam Singh <roxshivamsingh@gmail.com>
2024-06-20 17:43:00 +0530

    FIX: Cms sign-out button UI;

--- app/cms/containers/kitchens/components/Header.tsx
+++ app/cms/containers/kitchens/components/Header.tsx
@@ -5,26 +5,40 @@ import { useAppDispatch, useAppSelector } from '../../../../../redux'
 import _ from 'lodash';
 import { useFirebaseCmsAuthAction } from '../../../utils/firebase/use-firebase-cms-actions';
 import { setAuth } from '../../../redux/slices/Auth.slice';
+import { useCallback } from 'react';
 const Header = () => {
     const auth = useAppSelector(({ Cms }) => Cms.Auth.value);
     const AuthAction = useFirebaseCmsAuthAction()
     const navigate = useNavigate()
     const dispatch = useAppDispatch()
+    const onClickSignOut = useCallback(() => {
+        AuthAction.signOut()
+        dispatch(setAuth(undefined))
+        navigate('/cms/sign-in')
+    }, [AuthAction, dispatch, navigate])
     return (<nav className="bg-white border-gray-200 border-b-2">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <Link to="/">
-                <span className="self-center text-2xl font-semibold whitespace-nowrap uppercase">
-                    Magppie
-                </span>
-            </Link>
-            <p className="font-medium flex flex-col p-0 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
-                Welcome, {_.get(auth, 'name', 'User')}
-            </p>
-            <MdLogout onClick={() => {
-                AuthAction.signOut()
-                dispatch(setAuth(undefined))
-                navigate('/cms/sign-in')
-            }} />
+            <div className="">
+                <Link to="/">
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap uppercase">
+                        Magppie
+                    </span>
+                </Link>
+            </div>
+            <div className="flex flex-row justify-center align-middle gap-3 ">
+                <div className="">
+                    <p className="font-medium flex flex-col p-0 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
+                        Welcome, {_.get(auth, 'name', 'User')}
+                    </p>
+                </div>
+
+                <div className="m-auto">
+                    <MdLogout onClick={onClickSignOut}
+                        className='cursor-pointer hover:text-gray-500 '
+                    />
+
+                </div>
+            </div>
         </div>
     </nav>
     )
