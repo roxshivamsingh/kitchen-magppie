f973c14a388a244ce6319f1663ea0818e4bd474c
AkshayKDev <akshay.official.dev@gmail.com>
2024-06-22 02:26:08 +0530

    updated the ui forcms login

--- app/cms/containers/auth/SignIn/SignIn.tsx
+++ app/cms/containers/auth/SignIn/SignIn.tsx
@@ -3,43 +3,26 @@ import * as yup from 'yup'
 import { useForm } from 'react-hook-form'
 import { useNavigate } from 'react-router-dom'
 import { yupResolver } from '@hookform/resolvers/yup'
-// import { Icon } from '@iconify/react'
 import _ from 'lodash'
+import LoginBanner from '../../../../../assets/photos/kitchen/g5.png'
 
 import { useFirebaseCmsAuthAction } from '../../../../../app/cms/utils/firebase/use-firebase-cms-actions'
 import CircularProgress from '../../../../../components/CircularProgress'
+// import { Link } from 'react-router-dom'
 
 // import { useFirebaseCmsAuthListener } from '../../../utils/firebase/use-firebase-cms-listeners'
 
-
 export default function CmsSignIn() {
-    // const { watch } = useForm({ resolver: yupResolver(schema) })
-
-    // const AuthAction = useFirebaseCmsAuthAction()
-    // const values = watch()
-
-    // useFirebaseCmsAuthListener()
-
-    // const onSignUp = useCallback(() => {
-    //     if (values?.email?.length && values?.password?.length) {
-    //         AuthAction.signUp(values as TFormInput)
-    //     }
-    // }, [AuthAction, values])
-
     return (
-        <div className="min-h-screen flex md:flex-row ">
-            <div className="w-full md:w-1.2/4 flex items-center justify-center bg-gradient-to-r from-gray-100 to-gray-300 min-h-screen">
-                <div className="w-full max-w-md p-8 space-y-6 bg-white shadow-lg rounded-lg">
-                    <h2
-                        className="text-2xl font-bold text-center"
-                    // onClick={onSignUp}
-                    >
-                        CMS SignIn
-                    </h2>
-                    <SignInForm />
-                </div>
+        <div className="flex flex-col md:flex-row min-h-screen">
+            <SignInForm />
+            <div className="md:w-1/2 hidden md:block min-h-screen">
+                <img
+                    src={LoginBanner}
+                    alt="Placeholder"
+                    className="w-full h-full object-cover"
+                />
             </div>
-
         </div>
     )
 }
@@ -73,142 +56,103 @@ export function SignInForm() {
     const onSubmit = handleSubmit((data: TFormInput) => {
         setValue('loading', true)
         setTimeout(() => {
-            AuthAction.signIn(data).then((e) => {
-                if (e?.user) {
-                    navigate('/cms')
-                }
-            }).catch(() => {
-                setError('root', { type: 'validate' })
-            }).finally(() => {
-                setValue('loading', false)
-            })
+            AuthAction.signIn(data)
+                .then((e) => {
+                    if (e?.user) {
+                        navigate('/cms')
+                    }
+                })
+                .catch(() => {
+                    setError('root', { type: 'validate' })
+                })
+                .finally(() => {
+                    setValue('loading', false)
+                })
         }, 200)
     })
     const renderSubmitButton = (
         <button
             type="submit"
             disabled={values?.loading}
-            className={`grid grid-cols-3 flex-row align-middle w-full py-3 px-4 ${isError ? 'bg-red-600' : 'bg-black'
-                } uppercase text-white ${isError ? 'hover:bg-red-700' : 'bg-black'
-                }  focus:outline-none focus:ring-2 ${isError ? 'focus:ring-red-500' : 'bg-black'
-                } focus:ring-offset-2`}
+            className={`text-white w-full text-center h-12 bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex items-center me-2 mb-2 ${
+                isError ? 'bg-red-600' : 'bg-black'
+            } uppercase text-white ${
+                isError ? 'hover:bg-red-700' : 'bg-black'
+            }  focus:outline-none focus:ring-2 ${
+                isError ? 'focus:ring-red-500' : 'bg-black'
+            } focus:ring-offset-2`}
         >
-            <div className="" />
-            <div className="">Sign In</div>
-            <div className="">
-                {values.loading && (
-
-                    <CircularProgress size='xl' />
-                )}
+            <div className="flex justify-center items-center w-full">
+                <div>Sign In</div>
+                <div className="">
+                    {values.loading && <CircularProgress size="xl" />}
+                </div>
             </div>
         </button>
     )
 
     return (
-        <form onSubmit={onSubmit} className="space-y-4 "
-
-            onKeyUp={(e) => {
-                if (e.code === 'Enter') {
-                    onSubmit()
-                }
-            }}
-        >
-            {/* <form>
-                <div className="mb-4">
-                    <div className="relative">
+        <div className="md:w-1/2 bg-white md:flex md:justify-center md:items-center min-h-screen pt-60 w-full md:pt-0">
+            <div className="max-w-sm w-full px-6 py-8">
+                <h2 className="text-center font-semibold text-2xl mb-4 uppercase">
+                    sign up
+                </h2>
+                <form
+                    onSubmit={onSubmit}
+                    className="space-y-4 w-full"
+                    onKeyUp={(e) => {
+                        if (e.code === 'Enter') {
+                            onSubmit()
+                        }
+                    }}
+                >
+                    <div>
                         <input
+                            id="email"
+                            name="email"
                             type="email"
+                            {...register('email')}
                             placeholder="Email"
-                            className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none"
+                            className={`bg-gray-50 border h-12 border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${
+                                errors.email
+                                    ? 'border-red-500'
+                                    : 'border-gray-400'
+                            }`}
                         />
-                        <span className="absolute right-4 top-4">
-                            <svg
-                                className="fill-current"
-                                width={22}
-                                height={22}
-                                viewBox="0 0 22 22"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <g opacity="0.5">
-                                    <path
-                                        d="M19.2516 3.30005H2.75156C1.58281 3.30005 0.585938 4.26255 0.585938 5.46567V16.6032C0.585938 17.7719 1.54844 18.7688 2.75156 18.7688H19.2516C20.4203 18.7688 21.4172 17.8063 21.4172 16.6032V5.4313C21.4172 4.26255 20.4203 3.30005 19.2516 3.30005ZM19.2516 4.84692C19.2859 4.84692 19.3203 4.84692 19.3547 4.84692L11.0016 10.2094L2.64844 4.84692C2.68281 4.84692 2.71719 4.84692 2.75156 4.84692H19.2516ZM19.2516 17.1532H2.75156C2.40781 17.1532 2.13281 16.8782 2.13281 16.5344V6.35942L10.1766 11.5157C10.4172 11.6875 10.6922 11.7563 10.9672 11.7563C11.2422 11.7563 11.5172 11.6875 11.7578 11.5157L19.8016 6.35942V16.5688C19.8703 16.9125 19.5953 17.1532 19.2516 17.1532Z"
-                                        fill=""
-                                    />
-                                </g>
-                            </svg>
-                        </span>
+                        {helperText('email')}
                     </div>
-                </div>
-                <div className="mb-6">
-
-                    <div className="relative">
+                    <div>
                         <input
+                            id="password"
+                            name="password"
+                            {...register('password')}
                             type="password"
                             placeholder="Password"
-                            className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                            className={`bg-gray-50 border h-12 border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${
+                                errors.password
+                                    ? 'border-red-500'
+                                    : 'border-gray-400'
+                            }`}
                         />
-                        <span className="absolute right-4 top-4">
-                            <svg
-                                className="fill-current"
-                                width={22}
-                                height={22}
-                                viewBox="0 0 22 22"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <g opacity="0.5">
-                                    <path
-                                        d="M16.1547 6.80626V5.91251C16.1547 3.16251 14.0922 0.825009 11.4797 0.618759C10.0359 0.481259 8.59219 0.996884 7.52656 1.95938C6.46094 2.92188 5.84219 4.29688 5.84219 5.70626V6.80626C3.84844 7.18438 2.33594 8.93751 2.33594 11.0688V17.2906C2.33594 19.5594 4.19219 21.3813 6.42656 21.3813H15.5016C17.7703 21.3813 19.6266 19.525 19.6266 17.2563V11C19.6609 8.93751 18.1484 7.21876 16.1547 6.80626ZM8.55781 3.09376C9.31406 2.40626 10.3109 2.06251 11.3422 2.16563C13.1641 2.33751 14.6078 3.98751 14.6078 5.91251V6.70313H7.38906V5.67188C7.38906 4.70938 7.80156 3.78126 8.55781 3.09376ZM18.1141 17.2906C18.1141 18.7 16.9453 19.8688 15.5359 19.8688H6.46094C5.05156 19.8688 3.91719 18.7344 3.91719 17.325V11.0688C3.91719 9.52189 5.15469 8.28438 6.70156 8.28438H15.2953C16.8422 8.28438 18.1141 9.52188 18.1141 11V17.2906Z"
-                                        fill=""
-                                    />
-                                    <path
-                                        d="M10.9977 11.8594C10.5852 11.8594 10.207 12.2031 10.207 12.65V16.2594C10.207 16.6719 10.5508 17.05 10.9977 17.05C11.4102 17.05 11.7883 16.7063 11.7883 16.2594V12.6156C11.7883 12.2031 11.4102 11.8594 10.9977 11.8594Z"
-                                        fill=""
-                                    />
-                                </g>
-                            </svg>
-                        </span>
+                        {helperText('password')}
                     </div>
-                </div>
-                <div className="mb-5">
-                    <input
-                        type="submit"
-                        defaultValue="Sign In"
-                        className="w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 font-medium text-white transition hover:bg-opacity-90 bg-black"
-                    />
-                </div>
-
-            </form> */}
-            <div>
-                <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    {...register('email')}
-                    placeholder="Email"
-                    className={`w-full text-xl border-t-0 border-l-0 border-r-0 ${errors.email ? 'border-red-500' : 'border-gray-400'
-                        }`}
-                />
-                {helperText('email')}
+                    {/* <div className="text-right text-sm cursor-pointer">
+                        Forgot Password
+                    </div> */}
+                    <div>{renderSubmitButton}</div>
+                </form>
+                {/* <div className="text-center mt-4">
+                    <p>
+                        Already have an account?
+                        <Link to="/portal/register">
+                            <span className="text-blue-500 cursor-pointer ml-2">
+                                Sign In
+                            </span>
+                        </Link>
+                    </p>
+                </div> */}
             </div>
-            <div>
-                <input
-                    id="password"
-                    name="password"
-                    {...register('password')}
-                    type="password"
-                    placeholder="Password"
-                    className={`w-full text-xl border-t-0 border-l-0 border-r-0 ${errors.password ? 'border-red-500' : 'border-gray-400'
-                        }`}
-                />
-                {helperText('password')}
-            </div>
-            <div className="text-right text-sm cursor-pointer">
-                Forgot Password
-            </div>
-            <div>{renderSubmitButton}</div>
-        </form>
+        </div>
     )
 }
 
