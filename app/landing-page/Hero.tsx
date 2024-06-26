b0fba8b21bb052f577ee11894f71df62e488a809
AkshayKDev <akshay.official.dev@gmail.com>
2024-06-27 00:10:22 +0530

    added landing page route and design

---
+++ app/landing-page/Hero.tsx
@@ -0,0 +1,24 @@
+const Hero = () => {
+    return (
+        <div
+            className="relative bg-cover bg-center h-screen"
+            style={{ backgroundImage: `url('../../assets/photos/review.png')` }}
+        >
+            <div className="absolute inset-0 flex items-center justify-center">
+                <div className="text-center text-white px-4">
+                    <h1 className="text-6xl lg:text-7xl font-bold mb-4">
+                        MAGPPIE
+                    </h1>
+                    <p className="lg:text-4xl text-lg mb-4">
+                        an exclusive compilation for Ms Monica Chaudhary
+                    </p>
+                    <p className="text-sm lg:text-xl font-bold">
+                        Habitat Architects • India
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
