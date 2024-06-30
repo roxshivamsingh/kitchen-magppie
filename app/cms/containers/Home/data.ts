cbc030cdc0bb02118469ebb55b21401d6c0fd689
AkshayKDev <akshay.official.dev@gmail.com>
2024-07-01 00:53:18 +0530

    Added customer module ui in cms

--- app/cms/containers/Home/data.ts
+++ app/cms/containers/Home/data.ts
@@ -1,9 +1,11 @@
 import G12 from '../../../../assets/photos/kitchen/g12.png'
 import G10 from '../../../../assets/photos/kitchen/g10.png'
 import G4 from '../../../../assets/photos/kitchen/g4.png'
+import G14 from '../../../../assets/photos/kitchen/g14.png'
 
 export const CmsHomeData = [
     { id: '1', link: '/cms/kitchens', image: G12, title: 'Kitchens' },
     { id: '2', link: '/cms/users', image: G10, title: 'Users' },
     { id: '3', link: '/cms/projects', image: G4, title: 'Projects' },
+    { id: '4', link: '/cms/customer', image: G14, title: 'Customer' },
 ]
