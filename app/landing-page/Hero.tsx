e1fdc6d2abead2c4e540400117893440eb5be79e
Shivam Singh <roxshivamsingh@gmail.com>
2024-07-02 14:31:11 +0530

    ADD: INIT_COMPONENT;

--- app/landing-page/Hero.tsx
+++ app/landing-page/Hero.tsx
@@ -1,4 +1,3 @@
-// import { db, firestore } from "../../config/firebase.config";
 import { TComponentItem } from "../../types/component"
 
 type TProps = { data: TComponentItem }
@@ -6,27 +5,19 @@ export default function Hero({ data }: TProps) {
     return (
         <div
             className="relative bg-cover bg-center h-screen"
-            style={{ backgroundImage: `url(${data.links.bg})` }}
+            style={{ backgroundImage: `url(${data?.links?.bg})` }}
         >
+
             <div className="absolute inset-0 flex items-center justify-center">
                 <div className="text-center text-white px-4">
-                    <h1 className="text-6xl lg:text-7xl font-bold mb-4"
-
-                    // onClick={() => {
-                    //     const { addDoc, collection } = firestore
-                    //     _CUSTOMER_SITE_COMPONENTS?.forEach((row) => {
-                    //         addDoc(collection(db, 'customer-site-components'), row)
-
-                    //     })
-                    // }}
-                    >
-                        {data.typography.main}
+                    <h1 className="text-6xl lg:text-7xl font-bold mb-4">
+                        {data?.typography?.main}
                     </h1>
                     <p className="lg:text-4xl text-lg mb-4">
-                        {data.typography.secondary}
+                        {data?.typography?.secondary}
                     </p>
                     <p className="text-sm lg:text-xl font-bold">
-                        {data.typography.subtitle}
+                        {data?.typography?.subtitle}
                     </p>
                 </div>
             </div>
--- app/landing-page/LandingPage.tsx
+++ app/landing-page/LandingPage.tsx
@@ -1,6 +1,6 @@
 import PageProgress from '../../components/PageProgress'
 import { useAppSelector } from '../../redux'
-import { useFirebaseCustomerListener } from '../cms/utils/firebase/customer/use-firebase-customer-listeners'
+import { useFirebaseCustomerListener } from '../cms/utils/firebase/customer'
 import About from './About'
 import Caption from './Caption'
 import Hero from './Hero'
@@ -9,14 +9,14 @@ import Intro from './Intro'
 export default function LandingPage() {
 
     useFirebaseCustomerListener()
+
     const data = useAppSelector((state) => state.Cms.CustomerSiteComponent);
 
     if (data.status === 'loading') {
         return <PageProgress />
     }
-
-    return (
-        <>
+    if (data?.value?.length)
+        return (<>
             <Hero data={data.value?.find((row) => row.value === 'first-component')} />
             <About
                 data={data.value?.find((row) => row.value === 'planet-component')}
@@ -25,7 +25,7 @@ export default function LandingPage() {
             <Caption
                 data={data.value?.find((row) => row.value === 'transform-component')}
             />
-        </>
-    )
+        </>)
+    return <>Components not found</>
 }
 
