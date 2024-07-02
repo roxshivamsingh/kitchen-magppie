00cfcb59e691fda24a169b1ac903ad027f5bd06b
AkshayKDev <akshay.official.dev@gmail.com>
2024-07-02 13:04:33 +0530

    small changes

--- app/landing-page/Caption.tsx
+++ app/landing-page/Caption.tsx
@@ -1,15 +1,16 @@
-import { TComponentItem } from "../../types/component";
+import { TComponentItem } from '../../types/component'
 
 type TProps = { data: TComponentItem }
 
 export default function Caption({ data }: TProps) {
-
     return (
         <div className="flex items-center justify-center h-screen bg-[#afa796]">
             <div className="text-center text-white">
                 <h1 className="text-6xl font-light mb-4">
                     {data.typography.main}{' '}
-                    <span className="underline">{data.typography.secondary}</span>
+                    <span className="underline">
+                        {data.typography.secondary}
+                    </span>
                 </h1>
                 <p className="text-4xl font-light">
                     {data.typography.subtitle}
@@ -18,4 +19,3 @@ export default function Caption({ data }: TProps) {
         </div>
     )
 }
-
--- app/landing-page/LandingPage.tsx
+++ app/landing-page/LandingPage.tsx
@@ -7,9 +7,8 @@ import Hero from './Hero'
 import Intro from './Intro'
 
 export default function LandingPage() {
-
     useFirebaseCustomerListener()
-    const data = useAppSelector((state) => state.Cms.CustomerSiteComponent);
+    const data = useAppSelector((state) => state.Cms.CustomerSiteComponent)
 
     if (data.status === 'loading') {
         return <PageProgress />
@@ -17,15 +16,26 @@ export default function LandingPage() {
 
     return (
         <>
-            <Hero data={data.value?.find((row) => row.value === 'first-component')} />
-            <About
-                data={data.value?.find((row) => row.value === 'planet-component')}
+            <Hero
+                data={data.value?.find(
+                    (row) => row.value === 'first-component'
+                )}
             />
-            <Intro data={data.value?.find((row) => row.value === 'rating-component')} />
+            <About
+                // data={data.value?.find(
+                //     (row) => row.value === 'planet-component'
+                // )}
+            />
+            <Intro
+                data={data.value?.find(
+                    (row) => row.value === 'rating-component'
+                )}
+            />
             <Caption
-                data={data.value?.find((row) => row.value === 'transform-component')}
+                data={data.value?.find(
+                    (row) => row.value === 'transform-component'
+                )}
             />
         </>
     )
 }
-
