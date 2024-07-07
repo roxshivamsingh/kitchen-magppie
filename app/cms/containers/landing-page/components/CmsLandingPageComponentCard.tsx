c366406fc100868b065856e0b5d41e810c06d7e3
AkshayKDev <akshay.official.dev@gmail.com>
2024-07-07 23:53:06 +0530

    added small changes

--- app/cms/containers/landing-page/components/CmsLandingPageComponentCard.tsx
+++ app/cms/containers/landing-page/components/CmsLandingPageComponentCard.tsx
@@ -11,7 +11,7 @@ export default function CmsLandingPageComponentCard(props: TProps) {
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                     {props.item.name}
                 </h5>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 line-clamp-1">
                     {props.item.typography.description}
                 </p>
                 <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
