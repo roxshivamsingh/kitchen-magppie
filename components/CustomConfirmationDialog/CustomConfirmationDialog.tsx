d0ade4f6c3d468f331f6a8c9cc822521a4d23583
Shivam Singh <roxshivamsingh@gmail.com>
2024-07-11 23:00:37 +0530

    FIX: Confirmation danger UI;

--- components/CustomConfirmationDialog/CustomConfirmationDialog.tsx
+++ components/CustomConfirmationDialog/CustomConfirmationDialog.tsx
@@ -14,9 +14,15 @@ export default function CustomConfirmationDialog(props: TProps) {
             <div className='text-gray-500  text-lg'>
                 {props?.text?.remark || 'Are you sure to do this?'}
             </div>
-            <div className='flex flex-row-reverse gap-1'>
+            <div className='flex justify-end gap-1'>
+                <button
+                    className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                    onClick={() => { props.onHide() }}
+                >
+                    Cancel
+                </button>
                 {props?.variant === 'danger' ? <button
-                    className="cursor-pointer focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 flex gap-1"
+                    className="cursor-pointer focus:outline-none text-white bg-red-500 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900 flex gap-1"
                     onClick={() => { props.onConfirm() }}
                 >
                     <MdDeleteOutline className="text-xl" />
@@ -28,12 +34,7 @@ export default function CustomConfirmationDialog(props: TProps) {
                     {props?.text?.buttonLabel || 'Submit'}
                 </button>}
 
-                <button
-                    className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                    onClick={() => { props.onHide() }}
-                >
-                    Cancel
-                </button>
+
             </div>
         </div>
     </CustomSimpleModal>
