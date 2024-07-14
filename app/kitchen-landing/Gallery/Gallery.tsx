033de5c36526befb797183077c2da9359fd2bbf9
AkshayKDev <akshay.official.dev@gmail.com>
2024-07-15 00:34:50 +0530

    added swiper

--- app/kitchen-landing/Gallery/Gallery.tsx
+++ app/kitchen-landing/Gallery/Gallery.tsx
@@ -1,26 +1,38 @@
-import { Carousel } from 'react-responsive-carousel'
 import { galleries } from './components/data'
-// import Card from './components/Card'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Scrollbar } from 'swiper/modules'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/scrollbar'
 
 const Gallery = () => {
     return (
-        <div className="px-8 py-10 flex flex-col items-start bg-[#f9f5ef]">
+        <div className="px-2 py-10 flex flex-col items-start bg-[#f9f5ef]">
             <h1 className="mb-10 text-7xl">
                 Explore our <span className="italic">Gallery</span>
             </h1>
-            <Carousel
-                showArrows={true}
-                showThumbs={false}
-                infiniteLoop={true}
-                autoPlay={true}
-                interval={3000}
-                className="carousel-root w-full"
+            <Swiper
+                scrollbar={{
+                    hide: true,
+                }}
+                modules={[Scrollbar]}
+                className="w-full h-full" // Adjusted width and height
+                style={{ overflow: 'hidden' }} // Added overflow handling
             >
                 {galleries.map((feature, i) => (
-                    <div className="flex flex-col p-6 bg-white rounded-lg shadow-md" key={i}>
-                        <img src={feature.img} alt="logo" className="h-100 mb-4 object-cover" />
-                    </div>))}
-            </Carousel>
+                    <SwiperSlide
+                        key={i}
+                        className="flex justify-center items-center"
+                    >
+                        <img
+                            src={feature.img}
+                            alt="logo"
+                            className="h-screen object-cover"
+                        />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </div>
     )
 }
