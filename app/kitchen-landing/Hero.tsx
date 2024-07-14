50c59048cbbce403cf2446bd9374c2c3fb306555
AkshayKDev <akshay.official.dev@gmail.com>
2024-07-14 17:47:19 +0530

    hero changes

--- app/kitchen-landing/Hero.tsx
+++ app/kitchen-landing/Hero.tsx
@@ -11,23 +11,23 @@ const Hero = () => {
         >
             <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
             <div className="relative z-10 flex flex-col items-start justify-between h-full text-white">
-                <div className="flex items-center justify-between w-full px-6 py-4">
-                    <h1 className="text-white font-bold text-xl sm:text-4xl lg:text-3xl cursor-pointer">
+                <div className="flex items-center justify-between w-full px-6 py-4 mt-6">
+                    <h1 className="text-white font-bold text-2xl cursor-pointer">
                         MAGPPIE
                     </h1>
-                    <GiHamburgerMenu className="h-6 w-6 sm:h-10 sm:w-10 cursor-pointer" />
+                    <GiHamburgerMenu className="h-8 w-8 cursor-pointer" />
                 </div>
-                <div className="text-3xl sm:text-4xl mb-40 pl-6 sm:ml-8 p-4">
+                <div className="text-4xl mb-40 pl-6 p-4">
                     Welcome to the <span className="font-bold">SPIRITUAL</span>{' '}
                     heart of your Home.
                 </div>
             </div>
             <div className="absolute bottom-10 right-5 flex flex-col space-y-4 z-10">
-                <button className="p-3 sm:p-4 bg-[#202620] rounded-full text-white shadow-lg">
-                    <FaPhoneAlt className="h-5 w-5 sm:h-6 sm:w-6" />
+                <button className="p-3 bg-[#202620] rounded-full text-white shadow-lg">
+                    <FaPhoneAlt className="h-6 w-6" />
                 </button>
-                <button className="p-3 sm:p-4 bg-[#202620] rounded-full text-white shadow-lg">
-                    <RiUserSettingsFill className="h-5 w-5 sm:h-6 sm:w-6" />
+                <button className="p-3 bg-[#202620] rounded-full text-white shadow-lg">
+                    <RiUserSettingsFill className="h-6 w-6" />
                 </button>
             </div>
         </div>
