01b20ce5a6e4d904f4d3637c64da0b7d14d55c12
AkshayKDev <akshay.official.dev@gmail.com>
2024-07-14 17:13:34 +0530

    hero section changes

--- app/kitchen-landing/Hero.tsx
+++ app/kitchen-landing/Hero.tsx
@@ -12,12 +12,12 @@ const Hero = () => {
             <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
             <div className="relative z-10 flex flex-col items-start justify-between h-full text-white">
                 <div className="flex items-center justify-between w-full px-6 py-4">
-                    <h1 className="text-white font-bold text-xl sm:text-2xl lg:text-3xl">
+                    <h1 className="text-white font-bold text-xl sm:text-4xl lg:text-3xl cursor-pointer">
                         MAGPPIE
                     </h1>
-                    <GiHamburgerMenu className="h-6 w-6 sm:h-8 sm:w-8" />
+                    <GiHamburgerMenu className="h-6 w-6 sm:h-10 sm:w-10 cursor-pointer" />
                 </div>
-                <div className="text-3xl sm:text-4xl lg:text-5xl mb-16 sm:mb-20 lg:mb-40 ml-6 sm:ml-8 lg:ml-12 p-4">
+                <div className="text-3xl sm:text-4xl mb-40 pl-6 sm:ml-8 p-4">
                     Welcome to the <span className="font-bold">SPIRITUAL</span>{' '}
                     heart of your Home.
                 </div>
