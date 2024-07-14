17c103b8864c78aa12e351a39ed9ffd8366e3fe3
Shivam Singh <roxshivamsingh@gmail.com>
2024-07-15 00:20:48 +0530

    ADD: Landing > Footer;

--- app/kitchen-landing/LandingPage.tsx
+++ app/kitchen-landing/LandingPage.tsx
@@ -3,8 +3,11 @@ import Hero from './Hero'
 import Benefits from './Benefits/Benefits'
 import Clients from './Clients'
 import Gallery from './Gallery/Gallery'
-
-const LandingPage = () => {
+import { useAppSelector } from '../../redux'
+import { Footer } from "."
+export default function LandingPage() {
+    const components = useAppSelector((state) => state.Cms.Landing.value);
+    // console.log(components)
     return (
         <>
             <Hero />
@@ -12,8 +15,8 @@ const LandingPage = () => {
             <Benefits />
             <Clients />
             <Gallery />
+            <Footer item={components?.find((row) => row.name === 'footer-component')} />
         </>
     )
 }
 
-export default LandingPage
