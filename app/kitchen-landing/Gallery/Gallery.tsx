1a3030af32f837e6eb08285b70890625470e9180
Shivam Singh <roxshivamsingh@gmail.com>
2024-07-15 23:25:04 +0530

    ADD: Sticky navbar;

--- app/kitchen-landing/Gallery/Gallery.tsx
+++ app/kitchen-landing/Gallery/Gallery.tsx
@@ -7,32 +7,31 @@ import 'swiper/css/scrollbar'
 import { TComponentItem } from '../../../types'
 
 export function Gallery(props: TProps) {
-    return (
-        <div className="px-2 py-10 flex flex-col items-start bg-[#f9f5ef]">
-            <h1 className="mb-10 text-7xl"
-                dangerouslySetInnerHTML={{ __html: props.item.typography.main }}
-            />
+    return (<div className="px-2 py-10 flex flex-col items-start bg-[#f9f5ef]">
+        <h1 className="mb-10 text-7xl"
+            dangerouslySetInnerHTML={{ __html: props.item.typography.main }}
+        />
 
-            <Swiper
-                scrollbar={{ hide: true }}
-                modules={[Scrollbar]}
-                className="w-full h-100" // Adjusted width and height
-                style={{ overflow: 'hidden' }} // Added overflow handling
-            >
-                {props.item.gallery.map((feature, i) => (
-                    <SwiperSlide
-                        key={i}
-                        className="flex justify-center items-center"
-                    >
-                        <img
-                            src={feature.link}
-                            alt="logo"
-                            className="h-screen object-cover"
-                        />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-        </div>
+        <Swiper
+            scrollbar={{ hide: true }}
+            modules={[Scrollbar]}
+            className="w-full h-100" // Adjusted width and height
+            style={{ overflow: 'hidden' }} // Added overflow handling
+        >
+            {props.item.gallery.map((feature, i) => (
+                <SwiperSlide
+                    key={i}
+                    className="flex justify-center items-center"
+                >
+                    <img
+                        src={feature.link}
+                        alt="logo"
+                        className="h-screen object-cover"
+                    />
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    </div>
     )
 }
 
--- app/kitchen-landing/Hero.tsx
+++ app/kitchen-landing/Hero.tsx
@@ -3,20 +3,25 @@ import { RiUserSettingsFill } from 'react-icons/ri'
 // import heroBg from './assets/heroBg.png'
 import { GiHamburgerMenu } from 'react-icons/gi'
 import { TComponentItem } from '../../types'
+import { useMemo } from 'react'
 
 export function Hero(props: TProps) {
+    const renderNavigationBar = useMemo(() => {
+        return <div className="flex items-center justify-between w-full px-6 py-4 mt-6 fixed">
+            <h1 className="text-white z-100 font-bold text-6xl cursor-pointer mix-blend-difference">
+                {props.item.typography.main}
+            </h1>
+            <GiHamburgerMenu className="h-16 w-16 cursor-pointer" />
+        </div>
+    }, [props.item.typography.main])
     return (<div
         className="relative w-full h-screen bg-cover"
         style={{ backgroundImage: `url(${props.item.links.bg})` }}
     >
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
-        <div className="relative z-10 flex flex-col items-start justify-between h-full text-white">
-            <div className="flex items-center justify-between w-full px-6 py-4 mt-6">
-                <h1 className="text-white font-bold text-6xl cursor-pointer">
-                    {props.item.typography.main}
-                </h1>
-                <GiHamburgerMenu className="h-16 w-16 cursor-pointer" />
-            </div>
+
+        <div className="relative  flex flex-col items-start justify-between h-full text-white">
+            {renderNavigationBar}
             <div
                 className="text-8xl flex flex-col justify-center h-screen pl-6 p-4"
                 dangerouslySetInnerHTML={{
