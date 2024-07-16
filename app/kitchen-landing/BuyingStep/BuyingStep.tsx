3cf87df5e245680304662ddc4c0836cb3d43db08
AkshayKDev <akshay.official.dev@gmail.com>
2024-07-16 16:05:31 +0530

    added small changes

--- app/kitchen-landing/BuyingStep/BuyingStep.tsx
+++ app/kitchen-landing/BuyingStep/BuyingStep.tsx
@@ -5,24 +5,28 @@ export function BuyingStep(props: TProps) {
     return (
         <div className="bg-[#5d4e3e] text-white py-40 px-10 flex flex-col justify-start w-full">
             <div
-                className="text-6xl italic"
+                className="text-7xl italic"
                 dangerouslySetInnerHTML={{ __html: props.item.typography.main }}
             />
             <div className="grid grid-cols-2 mt-6">
                 {props.item.items?.map((item, i) => {
                     return (
                         <div key={i} className="flex flex-row gap-2 py-4 px-4">
-                            <div className="text-8xl italic">{i + 1}.</div>
-                            <div className="my-auto mt-10 max-w-sm text-2xl">
+                            <div className="text-8xl font-light italic">
+                                {i + 1}
+                            </div>
+                            <div className="my-auto mt-10 max-w-sm text-4xl font-light pl-2">
                                 {item.main}
                             </div>
                         </div>
                     )
                 })}
             </div>
-            <div className="inline-flex items-center text-4xl cursor-pointer mt-6">
-                {props.item.typography.action}{' '}
-                <FaArrowRight className="h-8 w-8 pl-3 pt-2" />
+            <div className="flex justify-start items-start">
+                <div className="text-3xl cursor-pointer mt-6 bg-brown-600 text-white font-light uppercase py-6 px-6 border border-white rounded-full flex items-center justify-start hover:bg-brown-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brown-500">
+                    {props.item.typography.action}{' '}
+                    <FaArrowRight className="h-8 w-8 pl-3 pt-2" />
+                </div>
             </div>
         </div>
     )
