2ae1bb40a6c016f0caed7899cbd8915c70b31d36
Shivam Singh <roxshivamsingh@gmail.com>
2024-07-16 20:37:36 +0530

    FIX: key error;

--- app/kitchen/containers/home/FAQ/FAQ.tsx
+++ app/kitchen/containers/home/FAQ/FAQ.tsx
@@ -8,19 +8,19 @@ export default function FAQ() {
             <h1 className="text-4xl font-medium mb-10">FAQ</h1>
             <Accordion className="p-2" collapseAll>
                 {faqData.map((data, index) => (
-                        <Accordion.Panel>
-                            <div className="p-2">
-                                <Accordion.Title>{data.title}</Accordion.Title>
-                                <Accordion.Content>
-                                    <p className="mb-2 mt-2 text-gray-500">
-                                        {data.description}
-                                    </p>
-                                </Accordion.Content>
-                            </div>
-                             {index !== faqData.length - 1 && (
+                    <Accordion.Panel key={index}>
+                        <div className="p-2">
+                            <Accordion.Title>{data.title}</Accordion.Title>
+                            <Accordion.Content>
+                                <p className="mb-2 mt-2 text-gray-500">
+                                    {data.description}
+                                </p>
+                            </Accordion.Content>
+                        </div>
+                        {index !== faqData.length - 1 && (
                             <hr className="border" />
                         )}
-                        </Accordion.Panel>
+                    </Accordion.Panel>
                 ))}
             </Accordion>
         </div>
