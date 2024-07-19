376dbd80b94c21aaff9212835d46128f3f710850
AkshayKDev <akshay.official.dev@gmail.com>
2024-07-20 01:31:07 +0530

    added more changes

--- app/kitchen-dweb/containers/Gallery/Gallery.tsx
+++ app/kitchen-dweb/containers/Gallery/Gallery.tsx
@@ -8,17 +8,18 @@ export function Gallery() {
                 className="mb-10 px-5 text-5xl font-extralight text-center"
                 dangerouslySetInnerHTML={{ __html: props.item.typography.main }}
             /> */}
-            <h1 className="text-5xl mb-5 text-center mt-10">
-                Explore our <span className="italic font-thin">Gallery</span>
+            <h1 className="text-5xl mb-5 text-center mt-10 font-extralight">
+                Explore our{' '}
+                <span className="italic font-thin uppercase">Gallery</span>
             </h1>
             {/* {props.item.gallery.map((feature, i) => ( */}
             {/* <> */}
-            <div className='container grid grid-cols-2 mx-auto max-w-7xl gap-5'>
+            <div className="grid grid-cols-2 gap-5 mx-10">
                 <img
                     // key={i}
                     src={gallerImg}
                     alt="logo"
-                    className="w-full h-full object-cover transition-transform duration-500 ease-in-out transform hover:scale-125"
+                    className="w-full h-full object-contain"
                 />
                 <img
                     // key={i}
