b6e6f3e46a278537e4eebb1c5395bd0a3acb6e3d
AkshayKDev <akshay.official.dev@gmail.com>
2024-07-20 18:25:20 +0530

    added new bg and video section

--- app/kitchen-dweb/components/Navbar.tsx
+++ app/kitchen-dweb/components/Navbar.tsx
@@ -1,20 +1,20 @@
-import { FaPhoneAlt } from 'react-icons/fa'
-import { RiUserSettingsFill } from 'react-icons/ri'
-import { TbMenu } from 'react-icons/tb'
+// import { FaPhoneAlt } from 'react-icons/fa'
+// import { RiUserSettingsFill } from 'react-icons/ri'
+// import { TbMenu } from 'react-icons/tb'
 
 const Navbar = () => {
     return (
-        <div className="w-full flex items-center justify-between h-24 px-10 bg-[#202620] text-white shadow-xl">
-            <TbMenu className="text-5xl cursor-pointer" />
-            <h1 className="text-4xl font-extrabold">MAGPPIE</h1>
-            <div className='flex gap-5'>
+        <div className="w-full flex items-center justify-center h-24 px-10 bg-[#202620] text-white shadow-xl">
+            {/* <TbMenu className="text-5xl cursor-pointer" /> */}
+            <h1 className="text-4xl font-bold text-center">MAGPPIE</h1>
+            {/* <div className='flex gap-5'>
                 <button className="p-2 text-white cursor-pointer">
                     <RiUserSettingsFill className="text-3xl" />
                 </button>
                 <button className="p-2 text-white cursor-pointer">
                     <FaPhoneAlt className="text-3xl" />
                 </button>
-            </div>
+            </div> */}
         </div>
     )
 }
--- app/kitchen-dweb/containers/Hero/Hero.tsx
+++ app/kitchen-dweb/containers/Hero/Hero.tsx
@@ -1,64 +1,35 @@
-import { useState } from 'react'
 import KitchenBg from '../../assets/hero-bg.png'
 import Consult from './Form'
 
 export function Hero() {
-    const [toggle, setToggle] = useState(INIT_TOGGLE)
-    return (<div
-        onClick={() => {
-            setToggle((prev) => ({ ...prev, isReelComponentClicked: true }))
-        }}
-        className="relative w-full bg-cover"
-        style={{ backgroundImage: `url(${KitchenBg})` }}
-    >
-        <div className="relative z-10 flex flex-col items-start justify-between h-full w-full">
-            {toggle.isReelComponentClicked ? (
-                <video
-                    className="object-cover w-full"
-                    autoPlay
-                    loop
-                    muted
-                    playsInline
-                >
-                    <source src={_data.bg.video} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
-            ) : (
-                <>
-                    <div className="absolute w-full h-full bg-black opacity-40" />
-                    <div className="flex items-center justify-between w-full h-screen container mx-auto max-w-7xl">
-                        <div className="flex">
-                            <h1 className="text-5xl font-extralight text-white z-20">
-                                Welcome to the <br />
-                                <span>
-                                    <span className="italic font-semibold">
-                                        {' '}
-                                        SPIRITUAL {' '}
-                                    </span>
-                                    heart
-                                </span>{' '}
-                                <br />
-                                of your Home.
-                            </h1>
-                        </div>
-
-                        <div className="z-20">
-                            <Consult />
-                        </div>
+    return (
+        <div
+            className="relative w-full bg-cover"
+            style={{ backgroundImage: `url(${KitchenBg})` }}
+        >
+            <div className="relative z-10 flex flex-col items-start justify-between h-full w-full">
+                <div className="absolute w-full h-full bg-black opacity-40" />
+                <div className="flex items-center justify-between w-full h-screen container mx-auto max-w-7xl">
+                    <div className="flex">
+                        <h1 className="text-5xl font-extralight text-white z-20">
+                            Welcome to the <br />
+                            <span>
+                                <span className="italic font-semibold">
+                                    {' '}
+                                    SPIRITUAL{' '}
+                                </span>
+                                heart
+                            </span>{' '}
+                            <br />
+                            of your Home.
+                        </h1>
                     </div>
-                </>
-            )}
+
+                    <div className="z-20">
+                        <Consult />
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
     )
-}
-
-const INIT_TOGGLE = {
-    isOpenContactModal: false,
-    isOpenConsultModal: false,
-    isReelComponentClicked: false,
-}
-
-const _data = {
-    bg: { video: "https://firebasestorage.googleapis.com/v0/b/magppie-e89d7.appspot.com/o/landing%2Fdesktop-reel.mp4?alt=media&token=a7eeb7dc-61e1-45c2-bed3-336d769ee064" }
 }
