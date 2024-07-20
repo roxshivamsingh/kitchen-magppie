b2085833e9e98d5ecefcdcfe79a1b59e3ddff29c
Shivam Singh <roxshivamsingh@gmail.com>
2024-07-20 20:48:54 +0530

    ADD: Card hover effect;

--- app/kitchen-dweb/containers/Features/Features.tsx
+++ app/kitchen-dweb/containers/Features/Features.tsx
@@ -1,21 +1,34 @@
+import { useState } from 'react'
 import { TComponentItem } from '../../../../types'
-
 export function Features(props: TProps) {
+    const [opacity, setOpacity] = useState(-1)
     return (
         <div className="flex flex-col items-center justify-center bg-[#f9f5ef] pt-10 w-full px-2">
             <h1 className="text-5xl font-light pb-10">
-                What makes{' '}
-                <span className="font-bold text-[#343b34]">MAGPPIE</span>{' '}
+                What makes <span className="font-bold text-[#343b34]">MAGPPIE</span>{' '}
                 different?
             </h1>
             <div className="flex container w-full mx-auto max-w-7xl gap-10">
                 {props.item.gallery.map((feature, i) => (
-                    <div className="flex w-full" key={i}>
+                    <div className={"flex w-full relative cursor-pointer group"} key={i}
+                        onMouseEnter={() => {
+                            setOpacity(i)
+                        }}
+                        onMouseLeave={() => {
+                            setOpacity(-1)
+                        }}
+                    >
                         <img
                             src={feature.link}
                             alt="logo"
                             className="w-full h-full"
                         />
+                        <div
+                            style={{ opacity: Number(i === opacity) }}
+                            className={`absolute inset-0 h-full w-full transition-opacity duration-500 ease-linear bg-[#262424c7]`}
+                        >
+                            <div className="text-white text-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">Did your kichen <span className='font-bold italic'>shower</span> today?</div>
+                        </div>
                     </div>
                 ))}
             </div>
