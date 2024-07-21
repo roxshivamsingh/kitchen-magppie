76cf9d4f91525df95979f89b9711fae8ad3a16de
Shivam Singh <roxshivamsingh@gmail.com>
2024-07-21 23:19:35 +0530

    FIX: Common site modal with scrolling hide;

--- app/kitchen-dweb/components/Modals/Contact.tsx
+++ app/kitchen-dweb/components/Modals/Contact.tsx
@@ -8,7 +8,7 @@ import {
 } from '../../types/request'
 import { useFirebaseRequestActions } from '../../../../appHooks/firebase/use-firebase-actions'
 import { ToastMessage } from '../../../../types'
-import { IoMdClose } from 'react-icons/io'
+import { CustomSiteModal } from '../../../../components'
 interface IProps {
     onHide: VoidFunction
     open: boolean
@@ -28,86 +28,53 @@ export function Contact(props: IProps) {
         toast(ToastMessage.Request.OK)
 
     })
-    return (
-        <>
-            <div
-                aria-hidden={!open}
-                className={`fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full overflow-y-auto overflow-x-hidden transform transition-transform duration-500 flex font-custom ${open ? 'translate-x-0' : '-translate-x-full'
-                    }`}
-            >
-                <div
-                    className={`fixed inset-0 transition-all duration-300 ${open ? 'bg-opacity-50' : 'bg-opacity-0'
-                        } backdrop-blur-sm`}
-                    onClick={() => {
-                        onHide()
-                    }}
+    return (<CustomSiteModal open={open} onHide={onHide} header='Request a Call Back' size='large'>
+        <form onSubmit={onSubmit}>
+            <div className="space-y-6">
+                <input
+                    placeholder="Your Name"
+                    type="text"
+                    name="fullName"
+                    {...register('fullName')}
+                    className="text-white text-md border rounded-full border-gray-300 bg-[#202620] focus:ring-white focus:border-white block w-full p-3 placeholder-gray-300 px-5"
+                    required
                 />
-                <div className="relative w-full max-w-2xl max-h-full">
-                    <div className="relative bg-[#202620] rounded-lg shadow">
-                        <div className="flex items-center justify-between px-6 py-5 rounded-t">
-                            <h3 className="text-4xl font-thin text-white">
-                                Request a Call Back
-                            </h3>
-                            <button
-                                type="button"
-                                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                onClick={onHide}
-                            >
-                                <IoMdClose className="h-12 w-12" />
-                            </button>
-                        </div>
-                        <div className="px-5 py-2">
-                            <form onSubmit={onSubmit}>
-                                <div className="space-y-6">
-                                    <input
-                                        placeholder="Your Name"
-                                        type="text"
-                                        name="fullName"
-                                        {...register('fullName')}
-                                        className="text-white text-md border rounded-full border-gray-300 bg-[#202620] focus:ring-white focus:border-white block w-full p-3 placeholder-gray-300 px-5"
-                                        required
-                                    />
-                                    <input
-                                        placeholder="Email ID"
-                                        type="email"
-                                        name="email"
-                                        {...register('email')}
-                                        className="text-white text-md border rounded-full border-gray-300 bg-[#202620] focus:ring-white focus:border-white block w-full p-3 placeholder-gray-300 px-5"
-                                        required
-                                    />
-                                    <input
-                                        placeholder="Mobile Number"
-                                        type="tel"
-                                        name="mobile"
-                                        id="mobile"
-                                        {...register('mobile')}
-                                        className="text-white text-md border rounded-full border-gray-300 bg-[#202620] focus:ring-white focus:border-white block w-full p-3 placeholder-gray-300 px-5"
-                                        required
-                                    />
-                                    <input
-                                        placeholder="Preferred Slot to Connect"
-                                        type="text"
-                                        name="remark"
-                                        {...register('remark')}
-                                        className="text-white text-md border rounded-full border-gray-300 bg-[#202620] focus:ring-white focus:border-white block w-full p-3 placeholder-gray-300 px-5"
-                                        required
-                                    />
-                                </div>
-                                <div className="flex flex-row-reverse mt-5">
-                                    <button
-                                        type="submit"
-                                        className=" text-black bg-white cursor-pointer font-medium text-md w-52 py-2 mb-2 text-center rounded-full uppercase"
-                                    >
-                                        Submit
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+                <input
+                    placeholder="Email ID"
+                    type="email"
+                    name="email"
+                    {...register('email')}
+                    className="text-white text-md border rounded-full border-gray-300 bg-[#202620] focus:ring-white focus:border-white block w-full p-3 placeholder-gray-300 px-5"
+                    required
+                />
+                <input
+                    placeholder="Mobile Number"
+                    type="tel"
+                    name="mobile"
+                    id="mobile"
+                    {...register('mobile')}
+                    className="text-white text-md border rounded-full border-gray-300 bg-[#202620] focus:ring-white focus:border-white block w-full p-3 placeholder-gray-300 px-5"
+                    required
+                />
+                <input
+                    placeholder="Preferred Slot to Connect"
+                    type="text"
+                    name="remark"
+                    {...register('remark')}
+                    className="text-white text-md border rounded-full border-gray-300 bg-[#202620] focus:ring-white focus:border-white block w-full p-3 placeholder-gray-300 px-5"
+                    required
+                />
             </div>
-        </>
-    )
+            <div className="flex flex-row-reverse mt-5">
+                <button
+                    type="submit"
+                    className=" text-black bg-white cursor-pointer font-medium text-md w-52 py-2 mb-2 text-center rounded-full uppercase"
+                >
+                    Submit
+                </button>
+            </div>
+        </form>
+    </CustomSiteModal>)
 }
 
 export default Contact
---
+++ components/CustomSiteModal/CustomSiteModal.tsx
@@ -0,0 +1,56 @@
+import { ReactNode, useEffect, useMemo } from 'react'
+import { IoMdClose } from 'react-icons/io'
+
+export default function CustomSiteModal(props: IProps) {
+
+    const isSmall = useMemo(() => props.size === 'small' || !props?.size?.length, [props.size])
+
+    useEffect(() => {
+        document.body.style.overflow = props.open ? 'hidden' : '';
+        return () => {
+            document.body.style.overflow = '';
+        };
+    }, [props.open]);
+
+    return (
+        <>
+            <div
+                className={`fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full overflow-y-auto overflow-x-hidden transform transition-transform duration-500 flex font-custom ${props.open ? 'translate-x-0' : '-translate-x-full'}`}>
+                <div
+                    className={`fixed inset-0 transition-all duration-300 backdrop-blur-sm ${props.open ? 'bg-opacity-50' : 'bg-opacity-0'}`}
+                    onClick={() => { props.onHide() }}
+                />
+                <div className={`relative w-full max-h-full ${isSmall ? 'max-w-3xl' : ' max-w-xl'}`}>
+                    <div className="relative bg-[#202620] rounded-lg shadow">
+                        <div className={`flex items-center justify-between rounded-t ${isSmall ? 'px-8 py-10' : 'px-6 py-5'}`}>
+                            <h3 className={`font-thin text-white ${isSmall ? 'text-7xl' : 'text-4xl'}`}>
+                                {props.header}
+                            </h3>
+                            {!isSmall && (
+                                <button
+                                    type="button"
+                                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                    onClick={props.onHide}
+                                >
+                                    <IoMdClose className="h-12 w-12" />
+                                </button>
+                            )}
+                        </div>
+
+                        <div className={isSmall ? "px-10 py-5" : "px-5 py-2"}>
+                            {props.children}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+interface IProps {
+    onHide: VoidFunction
+    open: boolean,
+    children: ReactNode,
+    header?: string
+    size?: 'small' | 'large'
+}
+
