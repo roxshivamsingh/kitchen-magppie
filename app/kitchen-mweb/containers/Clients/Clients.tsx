b7a74a1d2e658d6c2054ff528825820b5062e5cb
Shivam Singh <roxshivamsingh@gmail.com>
2024-07-30 23:55:36 +0530

    FIX: Minor refinement in FAQ and other;

--- app/kitchen-mweb/containers/Clients/Clients.tsx
+++ app/kitchen-mweb/containers/Clients/Clients.tsx
@@ -36,21 +36,18 @@ export function Clients() {
                 {clients.map((client) => (
                     // <SwiperSlide key={index}>
                     <div
-                        className={`relative w-full h-full overflow-hidden ${
-                            client.colSpan === 2 ? 'col-span-2' : 'col-span-1'
-                        } ${
-                            client.rowSpan === 2 ? 'row-span-2' : 'row-span-1'
-                        }`}
+                        className={`relative w-full h-full overflow-hidden ${client.colSpan === 2 ? 'col-span-2' : 'col-span-1'
+                            } ${client.rowSpan === 2 ? 'row-span-2' : 'row-span-1'
+                            }`}
                         key={client.id}
                     >
                         <img
                             src={client.src}
                             alt=""
-                            className={`w-full h-auto object-cover rounded-lg hover:rounded-lg ${
-                                client.height === 1
-                                    ? 'h-[350px] object-cover'
-                                    : ''
-                            }`}
+                            className={`w-full h-auto object-cover rounded-lg hover:rounded-lg ${client.height === 1
+                                ? 'h-[350px] object-cover'
+                                : ''
+                                }`}
                         />
                         <h3 className="text-start mt-2 text-3xl font-normal">
                             {client.title}
