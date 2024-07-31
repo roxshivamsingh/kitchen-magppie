b90ecd3f612cc7c4cd4bd98f9a90b3768b3152f4
Shivam Singh <roxshivamsingh@gmail.com>
2024-08-01 01:41:19 +0530

    ADD: Dropdown & Autocomplete UI;

---
+++ mocks/landing/consult.ts
@@ -0,0 +1,19 @@
+export const CONSULT_CITIES = [
+    "Mumbai",
+    "Delhi",
+    "Bangalore",
+    "Hyderabad",
+    "Ahmedabad",
+    "Chennai",
+    "Kolkata",
+    "Surat",
+    "Pune",
+    "Jaipur"
+    // Add more cities as needed
+];
+export const CONSULT_TENTATIVE_BUDGETS = [
+    '0 - 5 Lakhs',
+    '5 - 10 Lakhs',
+    '10 - 15 Lakhs',
+    '15 - 20 Lakhs'
+]
