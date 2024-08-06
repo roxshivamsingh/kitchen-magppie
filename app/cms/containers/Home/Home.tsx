a6008b5be508125868d4c31f32f209a7124259f1
AkshayKDev <akshay.official.dev@gmail.com>
2024-08-06 17:24:15 +0530

    added table for data in cms

--- app/cms/containers/Home/Home.tsx
+++ app/cms/containers/Home/Home.tsx
@@ -5,31 +5,41 @@ import { FaArrowRight } from 'react-icons/fa'
 
 export default function CmsHome() {
     const navigate = useNavigate()
-    return (<div className="container mx-auto items-center flex flex-col justify-center">
-        <h1 className="text-3xl mb-8 md:mb-0 font-bold uppercase text-center mt-20">
-            Dashboard
-        </h1>
-        <div className="grid grid-cols-1 md:grid-cols-3 space-x-2 lg:mt-10 lg:grid-cols-4 md:mt-10 gap-10">
-            {CmsHomeData.map((data, i) => (
-                <Card
-                    onClick={() => {
-                        navigate(data.link)
-                    }}
-
-                    className="cursor-pointer flex justify-center items-center"
-                    key={i}
-                    renderImage={() => (<img src={data.image} alt="logo" className="h-80 w-80" />)}
-                >
-                    <Link to={data.link} className='flex items-center'>
-                        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            {data.title}
-                        </h5>
-                        <FaArrowRight className='mt-2 ml-2' />
-                    </Link>
-                </Card>
-            ))}
+    return (
+        <div className="container mx-auto items-center flex flex-col">
+            <h1 className="text-3xl mb-8 md:mb-0 font-bold uppercase text-center mt-16">
+                Dashboard
+            </h1>
+            <div className="grid grid-cols-1 md:grid-cols-3 space-x-2 lg:mt-10 lg:grid-cols-4 md:mt-10 gap-10">
+                {CmsHomeData.map((data, i) => (
+                    <Card
+                        onClick={() => {
+                            navigate(data.link)
+                        }}
+                        className="cursor-pointer flex justify-center items-center"
+                        key={i}
+                        renderImage={() => (
+                            <img
+                                src={data.image}
+                                alt="logo"
+                                className="h-80 w-80"
+                            />
+                        )}
+                    >
+                        <Link to={data.link} className="flex items-center">
+                            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                {data.title}
+                            </h5>
+                            <FaArrowRight className="mt-2 ml-2" />
+                        </Link>
+                    </Card>
+                ))}
+            </div>
+            <Link to="/cms/enquiries">
+                <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-10 flex justify-center items-center w-full">
+                    View Enquiries
+                </button>
+            </Link>
         </div>
-    </div>
-
     )
 }
